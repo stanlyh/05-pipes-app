04f7f0f943769ba99a377cb5320cb3508ea23ec1
stanlyh <eduardostanly@gmail.com>
2025-07-30 22:38:08 -0500

    Instalación de Tailwind y creacion de pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'basic',
+    title: 'Pipes Basics',
+    loadComponent: () => import('./pages/basic-page/basic-page.component'),
+  },
+  {
+    path: 'numbers',
+    title: 'Pipes Numbers',
+    loadComponent: () => import('./pages/numbers-page/numbers-page.component'),
+  },
+  {
+    path: 'uncommon',
+    title: 'Pipes no tan comunes',
+    loadComponent: () => import('./pages/uncommon-page/uncommon-page.component'),
+  },
+  {
+    path: 'custom',
+    title: 'Pipes Personalizados',
+    loadComponent: () => import('./pages/custom-page/custom-page.component'),
+  },
+  {
+    path: '**',
+    redirectTo: 'basic',
+  }
+];
