f248de4cccbcdcaec2f0e8ddb4ac7a6cbab98915
stanlyh <eduardostanly@gmail.com>
2025-11-17 21:58:48 -0500

    Ejemplos DatePipe

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
@@ -11,5 +11,19 @@ export default class BasicPageComponent {
   nameLower = signal('celeste');
   nameUpper = signal('CELESTE');
   FullName = signal('EvaNGeliN CeleSTE');
+
+  customDate = signal( new Date() );
+
+  tickingDataEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000)
+
+    //limpiar intevalo
+    onCleanup(() => {
+      clearInterval(interval)
+    })
+  });
 
 }
