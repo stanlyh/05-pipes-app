9830d8f71fca09bb68a5578a2950768ae444f62d
stanlyh <eduardostanly@gmail.com>
2025-11-23 22:26:00 -0500

    Locale de la aplicación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeEs, 'es')
+registerLocaleData(localeFr, 'fr')
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    {
+      provide: LOCALE_ID,
+      useValue: 'fr',
+    }
+
+  ],
 };
