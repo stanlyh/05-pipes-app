9f17759b2bacd36681f8709dc7f68bd135bc1a4c
stanlyh <eduardostanly@gmail.com>
2025-12-02 21:06:13 -0500

    Ejemplo con i18nSelectPipe y i18nPluralPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../componentes/card/card.component";
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
+
+const client1 = {
+  name: 'Azul',
+  gender: 'male',
+  age: 39,
+  address: 'Ottawa, Canadá'
+}
+
+const client2 = {
+  name: 'Celeste',
+  gender: 'female',
+  age: 23,
+  address: 'Toronto, Canadá'
+}
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+  // i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
+
+  changeClient() {
+    if (this.client() == client1) {
+      this.client.set(client2);
+      return;
+    }
+    this.client.set(client1);
+  }
+
+  // i18n Plural
+  mininosMap = signal({
+    '=0': 'no tenemos ningun minino esperando',
+    '=1': 'tenemos un minino esperando',
+    '=2': 'tenemos 2 mininos esperando',
+    other: 'tenemos # mininos esperando',
+  });
+
+  mininos = signal([
+    'Celeste',
+    'Evangeline',
+    'Azul',
+    'Alely',
+    'Cristal',
+    'Flaca',
+    'Asraela',
+    'Caramelo',
+  ]);
+
+  deleteMinino() {
+    this.mininos.update((prev) => prev.slice(1));
+  }
+
+ }
