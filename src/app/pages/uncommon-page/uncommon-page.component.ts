b275ff85e4f33227e9c30b2ec2ef0ce45202bcb0
stanlyh <eduardostanly@gmail.com>
2025-12-15 23:57:33 -0500

    Ejemplo keyValuePipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../componentes/card/card.component";
-import { I18nPluralPipe, I18nSelectPipe, JsonPipe, SlicePipe, UpperCasePipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 const client1 = {
   name: 'Azul',
@@ -18,7 +18,7 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, UpperCasePipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, UpperCasePipe, KeyValuePipe, TitleCasePipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
@@ -60,6 +60,13 @@ export default class UncommonPageComponent {
 
   deleteMinino() {
     this.mininos.update((prev) => prev.slice(1));
+  }
+
+  // keyValue Pipe
+  profile ={
+    name: 'Azul Pelucho',
+    age: 1,
+    address: 'Aloasi',
   }
 
  }
