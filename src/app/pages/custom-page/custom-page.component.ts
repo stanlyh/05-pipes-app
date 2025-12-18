d6dfe57e74ca93908a00dc0dc5045d60f464b251
stanlyh <eduardostanly@gmail.com>
2025-12-17 19:12:32 -0500

    Diseño de una tabla para usar los pipes personalizados

--- src/app/pages/custom-page/custom-page.component.ts
+++ src/app/pages/custom-page/custom-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
 
 @Component({
   selector: 'app-custom-page',
@@ -10,4 +11,6 @@ export default class CustomPageComponent {
   name = signal('Celeste Cristal');
 
   upperCase = signal(true);
+
+  heroes = signal(heroes);
 }
